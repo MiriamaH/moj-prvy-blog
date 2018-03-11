9e9bd2d2aeae8f353b1bda7d0b6e8025749a8cdd
miriama <miriamahviscova@gmail.com>
2018-03-11 15:05:41 +0100

    My Django Girls app, first commit

---
+++ test.py
@@ -0,0 +1,45 @@
+print("ahoj!")
+ucastnicka = {'meno': 'mima', 'krajina': 'nemecko', 'mesto': 'bardejov'}
+print(ucastnicka['meno'])
+ucastnicka['vek']=25
+print(ucastnicka)
+if 3 > 2:
+    print('skvele')
+if 5 > 2:
+    print('5 je väčšie ako 2')
+else:
+    print('5 nie je väčšie ako 2')
+name = 'mima'
+if name == 'mima':
+    print('ahoj mima')
+elif name == 'mima':
+    print('ahoj mima')
+else:
+    print('ahoj cudzinec')
+def hi():
+    print('Ahoj')
+    print('ako sa mas?')
+hi()
+def hi(meno):
+    if meno == 'mima':
+        print('ahoj mima')
+    elif meno == 'sonja':
+        print('ahoj sonja')
+    else:
+        print('ahoj cudzinec')
+hi('mima')
+hi ('sonja')
+hi ('peter')
+def hi(meno):
+    print('Ahoj' + meno + '!')
+hi("Katka")
+dievčata = ['Katka', 'Monika', 'Zuzka', 'Ola', 'Ty']
+for meno in dievčata:
+    hi(meno)
+for i in range (1, 6):
+    print(i)
+
+C:\Users\Miriam PC\AppData\Local\Programs\Python\Python36\Scripts\;
+C:\Users\Miriam PC\AppData\Local\Programs\Python\Python36\;
+C:\Users\Miriam PC\AppData\Local\Programs\Python\Python37\Scripts\;
+C:\Users\Miriam PC\AppData\Local\Programs\Python\Python37\
